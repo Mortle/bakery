50d78c41b57885a481ba2db9f79c7f911092c647
Alexander Vinogradov <alexander.vinogradov.an@mail.ru>
2019-05-19 23:56:23 +0300

    feat: rescue some errors

--- bakery.rb
+++ bakery.rb
@@ -19,29 +19,37 @@ module Fides
       logger.info 'Bakery -- Start processing'
       @orders.each_with_index do |ord, index|
         logger.info "Bakery -- Processing order №#{index + 1}..."
+        raise ArgumentError unless ord[:amount].scan(/\D/).empty? # "x": if x is integer
         packs = @stock.select{ |product| ord[:code] == product[:code] }.map{|p| p[:amount].to_i}
-        output(make_change(ord[:amount].to_i, packs, ord[:code]), ord)
+        if packs.empty?
+          logger.info "Bakery -- Error at order №#{index + 1}: perhaps no specified product ID"
+        else
+          output(make_change(ord[:amount].to_i, packs, ord[:code]), ord)
+          logger.info "Bakery -- Order №#{index + 1} has been processed successfully!"
+        end
+      rescue ArgumentError, SystemStackError
+        logger.info "Bakery -- Error at order №#{index + 1}: perhaps wrong amount data / amount is too big to be processed"
       end
       logger.info 'Bakery -- End processing'
     end
 
     def make_change(amount, packs, code)
-     packs.sort! { |a, b| b <=> a }
-     optimal_change = Hash.new do |hash, key|
-       hash[key] = if key < packs.min
-         []
-       elsif packs.include?(key)
+      packs.sort! { |a, b| b <=> a }
+      optimal_change = Hash.new do |hash, key|
+      hash[key] = if key < packs.min
+        []
+      elsif packs.include?(key)
          [key]
-       else
+      else
          packs.
            reject { |pack| pack > key }.
            inject([]) {|mem, var| mem.any? { |p| p%var == 0 } ? mem : mem + [var]}.
            map { |pack| [pack] + hash[key - pack] }.
            reject { |change| change.sum != key }.
            min { |a, b| a.size <=> b.size } || []
-       end
-     end
-     optimal_change[amount].map{|amount| @stock.find{|p| p[:code] == code && p[:amount].to_i == amount }}
+      end
+      end
+    optimal_change[amount].map{|amount| @stock.find{|p| p[:code] == code && p[:amount].to_i == amount }}
     end
 
     def output(packs, order)
